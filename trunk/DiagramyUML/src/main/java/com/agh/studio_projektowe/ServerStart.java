04217024f083f4c23383ceefd6299c87ab5faba5
ddszczygiel <kotkidwa6>
2015-07-20 16:04:46 +0200

    fixed jar packaging - running using jar

--- trunk/DiagramyUML/src/main/java/com/agh/studio_projektowe/ServerStart.java
+++ trunk/DiagramyUML/src/main/java/com/agh/studio_projektowe/ServerStart.java
@@ -1,16 +1,31 @@
 package com.agh.studio_projektowe;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 
-@SpringBootApplication
+@Configuration
 @ComponentScan("com.agh.studio_projektowe")
+@EnableAutoConfiguration
 public class ServerStart {
 
-        public static void main(String[] args) throws Exception {
-            ConfigurableApplicationContext applicationContext = SpringApplication.run(TestClass.class, args);
-        }
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.setPort(8080);
+        factory.setSessionTimeout(1, TimeUnit.MINUTES);
+        //factory.addErrorPages(new ErrorPage(HttpStatus.404, "/notfound.html"));
+        return factory;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ServerStart.class, args);
+    }
 }
